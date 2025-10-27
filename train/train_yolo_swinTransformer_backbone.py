e33ac50294a2c958a2cfedae19a7f0d0ce7c1e4c
UltralyticsAssistant <web@ultralytics.com>
2025-10-27 04:47:37 +0000

    Auto-format by https://ultralytics.com/actions

--- train/train_yolo_swinTransformer_backbone.py
+++ train/train_yolo_swinTransformer_backbone.py
@@ -6,14 +6,15 @@ if __name__ == "__main__":
     os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
     # Load a model
-    model = YOLO('yolov8n-pose.pt')
+    model = YOLO("yolov8n-pose.pt")
 
     # Train the model
     model.train(
-        data='coco-pose.yaml',
+        data="coco-pose.yaml",
         epochs=3,
         imgsz=640,
         batch=16,
         # workers=4,
-        name='yolov8n-pose-swinTransformer',
-        exist_ok=True)
+        name="yolov8n-pose-swinTransformer",
+        exist_ok=True,
+    )
